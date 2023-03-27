53eadae2c6bf276271c1b0e456326a8dacdb2bbc
gabrielvieira-web <vieira.github@gmail.com>
2023-03-27 12:09:40 -0300

    projeto finalizado

--- projetos com js/imc/imc.js
+++ projetos com js/imc/imc.js
@@ -1,1 +1,40 @@
-window.alert('tudo certo')
+/*
+    Menor que 18 - Magreza
+    Entre 18 e 24 - Normal
+    Entre 25 e 29 - Sobrepeso
+    Entre 30 e 39 - Obesidade
+    Maior que 40 - Obesidade grave
+
+    Calculo IMC = peso / altura * altura
+*/
+
+let nome = document.getElementById('nome')
+let altura = document.getElementById('altura')
+let peso = document.getElementById('peso')
+let resultado = document.querySelector('#resultado')
+
+
+
+function calcular() {
+    if (nome.value.length == 0 || altura.value.length == 0 || peso.value.length == 0) {
+        resultado.innerHTML = 'OPS... Preencha todos os campos!'
+    } else {
+
+        let imc = peso.value / (altura.value * altura.value)
+        
+
+        if (imc < 18) {
+            resultado.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(1)} e você está abaixo do peso.`
+        } else if (imc >= 18 && imc <= 24) {
+            resultado.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(1)} e você está Normal.`
+        } else if (imc > 24 && imc <= 29) {
+            resultado.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(1)} e você está em Sobrepeso.`
+        } else if (imc > 29 && imc <= 39) {
+            resultado.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(1)} e você está em Obesidade.`
+        } else {
+            resultado.innerHTML = `${nome.value} seu IMC é ${imc.toFixed(1)} e você está em Obesidade grave.`
+        } 
+    }
+}
+
+
