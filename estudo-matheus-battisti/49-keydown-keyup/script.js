9b2c61a99fb66fe1355916c53a4c6205f7b46135
gabrielvieira-web <vieira.github@gmail.com>
2023-03-31 15:21:02 -0300

    .

---
+++ estudo-matheus-battisti/49-keydown-keyup/script.js
@@ -0,0 +1,19 @@
+// keydown
+
+document.addEventListener("keydown", function(event){
+
+    if(event.key === "Enter") {
+        console.log("Apertou Enter")
+    }
+
+})
+
+// keyup
+
+document.addEventListener("keyup", function(e){
+
+    if(event.key === "Enter") {
+        console.log("Soltou o Enter")
+    }
+})
+
